3df51c0bd2ff01f35761ca4427259a07a31755ed
shin <xinbai1992@gmail.com>
2017-01-24 19:43:51 +0800

    Three pointer.

---
+++ 26_removeDupFromSortArr/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int result = 1;
+        int j = 0,k = 0;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] != nums[j])
+            {
+                j = i;
+                result++;
+                k++;
+                nums[k] = nums[j];
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
