bf6317a471c1aca80ba52b7e69c8a1e147d70c0c
shin <xinbai1992@gmail.com>
2017-02-22 23:54:32 +0800

    minor fix

--- 121_bestTimetoBuyandSell/main.cpp
+++ 121_bestTimetoBuyandSell/main.cpp
@@ -11,7 +11,7 @@ public:
         int currMin = prices[0];
         for(int i = 1; i < prices.size(); i++)
         {
-            if(currMin < prices[i])
+            if(currMin > prices[i])
                 currMin = prices[i];
             result = result > prices[i] - currMin ? result : prices[i] - currMin;
         }
