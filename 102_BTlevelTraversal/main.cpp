bec75528a1e2590f453bf16e05386907d7f71924
shin <xinbai1992@gmail.com>
2017-02-23 22:22:29 +0800

    preorder slower

--- 102_BTlevelTraversal/main.cpp
+++ 102_BTlevelTraversal/main.cpp
@@ -12,38 +12,62 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution {
-public:
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "TemplateArgumentsIssues"
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        queue <TreeNode*> myQ;
-        vector<vector<int>> result;
-        if(root == NULL) return result;
-        myQ.push(root);
-        TreeNode * temp = NULL;
-        while(!myQ.empty())
+//class Solution {
+//public:
+//#pragma clang diagnostic push
+//#pragma ide diagnostic ignored "TemplateArgumentsIssues"
+//    vector<vector<int>> levelOrder(TreeNode* root) {
+//        queue <TreeNode*> myQ;
+//        vector<vector<int>> result;
+//        if(root == NULL) return result;
+//        myQ.push(root);
+//        TreeNode * temp = NULL;
+//        while(!myQ.empty())
+//        {
+//            vector<int> level;
+//            int size = myQ.size();
+//            for(int i = 0; i < size ;i++)
+//            {
+//                temp = myQ.front();
+//                if(temp->left != NULL)
+//                    myQ.push(temp->left);
+//                if(temp->right != NULL)
+//                    myQ.push(temp->right);
+//                level.push_back(temp->val);
+//                myQ.pop();
+//            }
+//            result.push_back(level);
+//        }
+//        return result;
+//    }
+//#pragma clang diagnostic pop
+//
+//};
+class Solution{
+public :
+    void helper(TreeNode* root, vector<vector<int>> & ret, int depth)
+    {
+        if(root == NULL) return;
+        if(depth == ret.size())
         {
-            vector<int> level;
-            int size = myQ.size();
-            for(int i = 0; i < size ;i++)
-            {
-                temp = myQ.front();
-                if(temp->left != NULL)
-                    myQ.push(temp->left);
-                if(temp->right != NULL)
-                    myQ.push(temp->right);
-                level.push_back(temp->val);
-                myQ.pop();
-            }
-            result.push_back(level);
+            vector<int> temp;
+            ret.push_back(temp);
         }
-        return result;
+
+        ret[depth].push_back(root->val);
+        helper(root->left,ret,depth+1);
+        helper(root->right,ret,depth+1);
     }
-#pragma clang diagnostic pop
 
+    vector<vector<int>> levelOrder(TreeNode* root)
+    {
+        vector<vector<int>> ret;
+        if(root == NULL) return ret;
+
+        helper(root,ret,0);
+        return ret;
+    }
 };
-
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
